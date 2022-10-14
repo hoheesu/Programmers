11f86bff4519b65415c896a3ffe798de4d54d5df
hoheesu <99115509+hoheesu@users.noreply.github.com>
2022-10-14 21:20:32 +0900

    [unrated] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.js
@@ -0,0 +1,14 @@
+function solution(angle) {
+    var answer = 0;
+    
+    if (angle < 90 && angle > 0){
+        return 1
+    }else if ( angle === 90 ){
+        return 2;
+    }else if ( angle === 180){
+        return 4;
+    }else{
+        return 3;
+    }
+    return answer;
+}
