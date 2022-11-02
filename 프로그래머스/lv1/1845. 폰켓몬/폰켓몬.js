19d57bdb83d39071580731573f10c1e82ba022ab
hoheesu <99115509+hoheesu@users.noreply.github.com>
2022-11-02 17:21:29 +0900

    [level 1] Title: 폰켓몬, Time: 5.70 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,14 @@
+function solution(nums) {
+    let answer = 0;
+    let nH = new Map()
+    for(const x of nums){
+        nH.set(x, (nums[x] || 0 ) +1)
+    }
+    console.log(nH.size);
+    if((nums.length / 2) > nH.size){
+        answer = nH.size
+    }else{
+        answer = nums.length / 2
+    }
+    return answer;
+}
