2237c921f84e9a8e146b684b37e22bbb2a1193af
hoheesu <99115509+hoheesu@users.noreply.github.com>
2022-11-11 16:59:49 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,28 @@
+function solution(nums) {
+    let answer = 0
+    let sum = []
+    let count = 0
+
+    for( let x = 0; x < nums.length; x++){
+        for( let xx = x+1; xx < nums.length; xx++){
+            if( x !== xx){
+                for( let xxx = xx+1; xxx < nums.length; xxx++){
+                    if( x!==xx && x !== xx && xx!==xxx){
+                        sum.push(nums[x]+nums[xx]+nums[xxx])
+                    }
+                }
+            }
+        }
+    }
+    // console.log(sum)
+    for( const num of sum){
+        let noPrime = 0
+        for(let i = 2; num > i; i++) {
+            if(num % i === 0) { //이 부분에서 num이  다른 수로 나눠떨어진다면 소수가 아님
+                noPrime ++
+            }
+        }
+        noPrime === 0 ? count ++ : count
+    }
+     return count
+}
