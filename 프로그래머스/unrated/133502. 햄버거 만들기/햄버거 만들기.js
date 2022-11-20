585bd42d4e9197b3bad8b467084b8ce4e6a8ec52
hoheesu <99115509+hoheesu@users.noreply.github.com>
2022-11-20 21:00:27 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,13 +1,13 @@
 function solution(ingredient) {
     let answer = 0;
     let stack = []
+    function stA (a){
+        return stack[stack.lenth - a]
+    }
     for( const x of ingredient){
         stack.push(x)
         let sl = stack.length
-        if (stack[sl-1] === 1 &&
-            stack[sl-2] === 3 &&
-            stack[sl-3] === 2 &&
-            stack[sl-4] === 1 ){
+        if (stack[sl-4] === 1 && stack[sl-3] === 2 && stack[sl-2] === 3 && stack[sl-1] === 1 ){
             stack.splice(sl-4, 4)
             answer++
         }
