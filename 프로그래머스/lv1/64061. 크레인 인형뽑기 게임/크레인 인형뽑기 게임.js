c13b929939511d0a4e383dd4fd36f105b15626e0
hoheesu <99115509+hoheesu@users.noreply.github.com>
2022-11-23 21:11:44 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.59 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,11 +1,3 @@
-// [0,0,0,0,0]
-// [0,0,1,0,3]
-// [0,2,5,0,1]
-// [4,2,4,4,2]
-// [3,5,1,3,1]
-
-// [1,5,3,5,1,2,1,4]
-
 function solution(board, moves) {
     let answer = 0;
     const bn = board.length
@@ -17,13 +9,11 @@ function solution(board, moves) {
             if( board[i][m] !== 0 ){ 
                 stack.push(board[i][m])
                 board[i][m] = 0
-                for( const s in stack){
-                    if(stack[s] === stack[s-1]){
+                    if(stack[stack.length-1] === stack[stack.length-2]){
                         stack.pop()
                         stack.pop()
                         answer+=2
                     }
-                }
                 break
             }
         }
