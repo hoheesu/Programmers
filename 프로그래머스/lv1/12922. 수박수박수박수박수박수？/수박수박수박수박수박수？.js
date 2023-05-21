d97d13a604cc6bd6681bdc79479ec02dc84cc68b
윤준수 <99115509+hoheesu@users.noreply.github.com>
2023-05-21 14:24:16 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -2,7 +2,7 @@ function solution(n) {
     let count = 0; 
     let answer = '';
     while( count < n ){
-        count%2===0 ? answer+='수' : answer+='박'
+        answer += (count%2===0) ? '수' : '박'
         count ++;
     }
     return answer;
