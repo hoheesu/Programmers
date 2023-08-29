4f3755157fa83bdc2ffd539ee6ac4417a900a44e
윤준수 <99115509+hoheesu@users.noreply.github.com>
2023-08-29 20:39:39 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,5 +1,5 @@
 function solution([...s]) {
     let Mid = Math.floor(s.length / 2)
-    return ( s.length % 2 === 1 ) ? s[Mid] : s[Mid-1] + s[Mid]
+    return s.length % 2 ? s[Mid] : s[Mid-1] + s[Mid]
     
 }
