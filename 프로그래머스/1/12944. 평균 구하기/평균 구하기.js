667d2b0d5bc720b0ecf47b4a2ff80e9871b7f5ca
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-02-13 16:05:09 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,8 +1,6 @@
 function solution(arr) {
     let answer = 0;
-    for( const x of arr){
-        answer += x
-    }
+    arr.map(x => answer += x)
     answer = (answer / arr.length)
     return answer;
 }
