5b55295ebff0193398ea08bc0c238a7b591ae5df
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-02-16 14:30:34 +0900

    [level 1] Title: 과일 장수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,14 @@
+function solution(k, m, score) {
+    let answer = 0;
+    score.sort((a,b) => b - a);
+    
+    // while(score.length >= m) {
+    //     answer += score.splice(score,m)[m - 1] * m
+    // }
+    for( let box = m-1; box < score.length; box += m){
+        answer += score[box] * m
+    }
+    
+    
+    return answer;
+}
