cf03c805c2ccda0f5339c2bec262330cf7309e98
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-02-16 20:09:02 +0900

    [level 1] Title: 모의고사, Time: 4.06 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,20 +1,26 @@
 function solution(answers) {
-    const students = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
-    let arr=[], result = [];
-    for(const i in students ){ // let i =0 ; i < students.length; i++
-        let check = 0;
-        for( const j in answers) {
-            if( students[i][j % students[i].length] === answers[j] ) check ++ //j % students[i].length
-        }
-        arr.push([i,check])
+ const students = {
+        1: [1, 2, 3, 4, 5],
+        2: [2, 1, 2, 3, 2, 4, 2, 5],
+        3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
     }
-    arr.sort( (a,b) => b[1] - a[1])
-    result.push(Number(arr[0][0])+1)
-    for(let i = 0; i < arr.length-1; i++ ) {
-        if ( arr[i][1] === arr[i + 1][1] ) {
-            result.push( Number(arr[i+1][0])+1 )
-        }else break
+    const result = []
+    const arr = {}
+    for (const student in students) {
+        let check = 0;
+        for (let i = 0; i < answers.length; i++) {
+            if (students[student][i % students[student].length] === answers[i]) {
+                check++;
+            }
+             // if( students[student][i] === answers[i] ) check ++
+        }
+        arr[student] = check;
     }
 
-    return result ;
+    const maxScore = Math.max(...Object.values(arr));
+    for (const key in arr) {
+        if (arr[key] === maxScore) result.push(+key);
+    }
+
+    return result;
 }
