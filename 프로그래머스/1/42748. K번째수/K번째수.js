f08b1b41d9e2db1525211a91e7ee7e28021043da
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-05-26 19:50:52 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,8 @@
 function solution(array, commands) {
     let answer = []
-    let arr = [];
-    for( const x of commands ){
-        arr = array.slice(x[0]-1,x[1])
-        arr.sort((a,b)=>a-b)
-        answer.push(arr[x[2]-1])
+    for( const x of commands ) {
+        const newArr = array.slice(x[0]-1, x[1]).sort((a,b)=> Number(a) - Number(b))
+        answer.push(newArr[x[2]-1])
     }
-    return answer;
+    return answer
 }
