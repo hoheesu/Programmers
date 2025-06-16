3f15e0e24fabedb2558c60ab94b436f9db63e127
윤준수 <99115509+hoheesu@users.noreply.github.com>
2025-06-16 22:11:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 62.83 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,14 +1,8 @@
 function solution(p, c) { 
-    let answer = ''
+    let answer = "";
     const hash = new Map()
-    for(const x of p){
-        hash.set(x, (hash.get(x) || 0 ) + 1);
-    }
-    for(const x of c){
-        hash.set(x, (hash.get(x) || 0) - 1)
-    }
-    hash.forEach((value, key) => {
-        if(value) answer = key
-    })
+    for ( const x of p ) {hash.set(x, ( hash.get(x) || 0) + 1 )}
+    for ( const x of c ) {hash.set(x, ( hash.get(x) || 0) - 1 )}
+    hash.forEach((value, key) => value ? answer = key : answer)
     return answer
 }
